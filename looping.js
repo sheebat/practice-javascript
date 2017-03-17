d2f508803cb7b74efdf1b4f947fc831fc98d382a
sheebat <sheeba.thukral@gmail.com>
2017-03-16 18:02:13 -0700

    Update looping.js

--- looping.js
+++ looping.js
@@ -8,3 +8,22 @@ do{
 var guess=prompt("Guess a number between 1 and 20");
 }while(guess!=15)
 alert("you guessed 15");
+
+for(j=0;j<=100;j++){
+  if((j%2)===0){
+  continue;
+}
+    if(j===19){
+      break;
+    }
+  document.write(j+"</br>")
+}
+
+var customer={name:"Bob Thomas", address:"123 Main", balance:50.50
+};
+for(k in customer){
+document.write(customer[k]+"</br>");
+}
+
+
+
