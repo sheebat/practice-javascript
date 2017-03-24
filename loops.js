41017b0f8829c8f8a0ec802bea4b84cbc074eeb0
sheebat <sheeba.thukral@gmail.com>
2017-03-23 22:37:27 -0700

    Create loops.js

---
+++ loops.js
@@ -0,0 +1,46 @@
+
+FOR-IF-LOOP
+var x=" ";
+var i;
+for(i=1;i<10;i++){
+if (i===3){break;}
+  x=x+"<h"+i+"/>Heading"+i+"</h"+i+">";
+}
+document.write(x);
+
+-------------------
+DO-WHILE LOOP
+var x=" ";
+i=1;
+
+do{
+  x=x+"<h"+i+"/>Heading"+i+"</h"+i+">";
+  i++
+}
+while(i<=6);
+document.write(x);
+------------------------------------------
+WHILE LOOP
+var x=" ";
+i=1;
+
+while(i<=6){
+  x=x+"<h"+i+"/>Heading"+i+"</h"+i+">";
+  i++
+}
+document.write(x);
+-------------------------------------
+FOR LOOP
+var x=" ";
+for(i=1;i<=6;i++){
+  x=x+"<h"+i+"/>Heading"+i+"</h"+i+">";
+  
+}
+document.write(x);
+------------------------------------
+
+var x=" ";
+for(i=0;i<5;i++){
+  x = x + "The number is " +i +"<br/>";
+}
+document.write(x + "<br/>");
